4cdba89d329c9a83da3dae1084e9a3cd771fb350
Kaitlin Schaer <kaitlin.schaer@gmail.com>
2018-07-19 12:09:34 -0400

    first commit, setting up with express and create react app

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+
+class App extends Component {
+  state = { users: [] };
+
+  async componentDidMount() {
+    // fetch("/users")
+    //   .then(res => res.json())
+    //   .then(users => this.setState({ users }));
+    // console.
+    try {
+      let res = await axios.get("/users");
+      let data = res.data;
+      console.log("what data??", data);
+    } catch (err) {
+      console.log("err!", err);
+    }
+  }
+
+  render() {
+    console.log();
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        {this.state.users.map(user => <div key={user.id}>{user.username}</div>)}
+      </div>
+    );
+  }
+}
+
+export default App;
